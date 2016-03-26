493d6490018b09a987223ae90a704b31b8bc8ace
elonus <andreas.matre@gmail.com>
2016-03-26 19:29:41 +0100

    Update testbot.py
    Added arithmetic functionality and rewrote how it listens for inputs

--- testbot.py
+++ testbot.py
@@ -1,11 +1,20 @@
-# -*- coding: utf-8 -*-
 import socket
 from time import sleep
+
+functions = {".math" : "argument arithmetic", "hello" : "noargument hello"} 
 
 def ping(data):
     irc.send( "PONG " + data.split() [ 1 ] + "\r\n" )
 
-def answer(data, message):
+def argument(data):
+    arg = ""
+    for i in data.split(":")[2].split()[1:]:
+        arg += i
+
+    return(arg) 
+
+
+def send(data, message):
     """
     sends message to channel or user depending on where it is from
     """
@@ -17,6 +26,39 @@ def answer(data, message):
 
     else:
         irc.send( "PRIVMSG " + sender + " :" + message + "\r\n")
+
+
+def hello(data):
+    sender = data.split("!")[0].strip(":")
+    answer = "Hello " + sender + "!"
+    send(data, answer)
+
+
+def arithmetic(data):
+    
+    expression = argument(data)
+    condition = "0123456789+-/*.()"
+    newexpression = ""
+    for i in expression:
+        if i in condition:
+            newexpression += str(i)
+
+    if ("**" in newexpression):
+        answer = "You tried to use a power. Unfortunately I do not have that functionnality at the moment."
+    if (len(newexpression) > 15):
+        answer = "Your expression was too long. I only accept expressions up to 15 characters long."
+
+    else:
+
+        try:
+            answer = eval("1.0 * " + newexpression)
+        except ZeroDivisionError:
+            answer = "Error! You tried to divide by zero"
+        except SyntaxError:
+            answer = "Error! Invalid input"
+
+    send(data, "The answer to " + expression + " is: " + str(answer))
+
 
 network = "irc.freenode.net"
 port = 6667
@@ -36,7 +78,6 @@ if data.find("PING"):
 irc.send ( "JOIN #elenusbottest\r\n" )
 sleep(2)
 irc.send ( "PRIVMSG #elenusbottest :Hello.\r\n" )
-#irc.send("PRIVMSG elonus :test\r\n")
 
 while True:
     data = irc.recv(4096).strip("\r\n")
@@ -47,7 +88,41 @@ while True:
 
     elif data.find("PRIVMSG") != -1:
         message = data.split(":")[2]
+        codeword = message.split()[0]
+        codeword = codeword.lower()
         sender = data.split("!")[0].strip(":")
-        message = message.capitalize()
-        if str(message) == "Hello":
-            answer(data, "hello " + sender)
+        
+        data2 = str(data)
+
+        if codeword in functions:
+
+            if functions[codeword].split()[0] == "argument":
+                try:
+                    message.split()[1]
+
+                except IndexError:
+                    send(data, codeword + " expects an argument")
+
+                else:
+                    eval(functions[codeword].split()[1] + "('" + data2 + "')")
+
+            else:
+                eval(functions[codeword].split()[1] + "('" + data2 + "')")
+
+
+
+"""
+        if codeword == "Hello":
+            send(data, "hello " + sender)
+
+        elif
+        
+        try:
+             message.split()[1]
+
+        except IndexError:
+             send(data, "This function needs an argument")
+
+        else:
+             for 
+"""
